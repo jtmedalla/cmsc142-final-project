ed7085e3a4f139f0bb633e26246fdd1dfc9cee2b
JuGo-CS <mondejarkenneth64@gmail.com>
2026-05-23 23:24:38 +0800

    feat: create a 'database' to store the items/foods

---
+++ src/core/db.py
@@ -0,0 +1,17 @@
+food_items_db = []
+
+def add_food_item(name, serving_size, value_serving, tolerance):
+    food_items_db[name] = {
+        "serving_size": float(serving_size),
+        "value_serving": float(value_serving),
+        "tolerance": int(tolerance)
+    }
+
+def delete_food_item_by_name(name):
+    food_items_db.pop(name, None)
+
+def get_all_food_items():
+    """
+    Returns the current list of all food items stored in the database.
+    """
+    return food_items_db
